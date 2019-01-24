b01a2f60bec0523435d6254fe60ccf0d5c22cdc3
Indre Bakeer <indre.bakeer@gmail.com>
2019-01-24 03:37:26 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -16,9 +16,8 @@ class Song
     song
   end
 
-  def self.new_by_name(name)
-    @name = self.create
-    @name
+  def self.new_by_name
+    song.name = self.create
   end
 
   def self.create_by_name
