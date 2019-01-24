b545c9b8426772128037438f0ea84bfbf8a01994
Indre Bakeer <indre.bakeer@gmail.com>
2019-01-24 04:40:57 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -41,8 +41,8 @@ class Song
      @@all.sort_by { |song| song.name }
   end
 
-  def self.new_from_filename
-
+  def self.new_from_filename(name)
+    song = self.name.split(".mp3")
   end
 
   def self.create_from_filename
